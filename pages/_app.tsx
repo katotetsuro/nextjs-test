8f117531f2d1ec37fb9833ce5baf01bf67e4849a
katotetsuro <katotetsuro@preferred.jp>
2020-11-21 22:34:20 +0900

    ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import { AppProps } from 'next/app'
 import '../styles/index.css'
+import * as gtag from '../lib/gtag'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return <Component {...pageProps} />
 }
