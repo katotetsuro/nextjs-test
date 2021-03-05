b40a2f75ed1c267891ea0be3c12e0b8116a00b83
katotetsuro <nistetsurooy@gmail.com>
2021-03-06 08:40:29 +0900

    lint:fix

--- components/post-preview.tsx
+++ components/post-preview.tsx
@@ -1,6 +1,4 @@
-import Avatar from './avatar'
 import DateFormatter from './date-formatter'
-import CoverImage from './cover-image'
 import Link from 'next/link'
 import Author from '../types/author'
 
